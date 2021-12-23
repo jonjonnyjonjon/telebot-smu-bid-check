3d1123f5ffcc47c0a0a45bb5d98283b09a6330c2
jonjonnyjonjon <70429099+jonjonnyjonjon@users.noreply.github.com>
2021-12-23 18:31:33 +0800

    feat: able to retrieve bids using specific search fields
    User is able to search for bids by giving the term, course code, bidding window and instruction (Option 1)

--- app.js
+++ app.js
@@ -1,35 +1,87 @@
 require("dotenv").config();
+const { Telegraf, Scenes, Markup, session } = require("telegraf");
 
-const { Telegraf } = require("telegraf");
 const axios = require("axios");
-const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.start(ctx => {
-    ctx.reply("Welcome to SMU Bid Checker bot. To check for bids, please send me: <course code>, <round and window number>, <instructor>. Example: IS215, Round 1B Window 1, Christopher Michael Poskitt.");   
+const { enter, leave } = Scenes.Stage;
+
+// User chose Text Bid Results
+const bidsInText = new Scenes.BaseScene("bidsInText");
+
+bidsInText.enter(ctx => {
+    ctx.replyWithHTML("How do you want to find your bids?\nOption 1: Specific term, course, bidding window and instructor\nOption 2: Specific term, course and bidding window", Markup.inlineKeyboard([
+        Markup.button.callback("Option 1", "option1"),
+        Markup.button.callback("Option 2", "option2")
+    ]))
+});
+bidsInText.action("option1", enter("bidsInTextOption1"))
+bidsInText.command("leave", ctx => {
+    ctx.reply("Bye bye! Thanks for using our bot.");
+    ctx.scene.leave();
 });
 
-bot.on("message", async (ctx) => {
-    const userInput = ctx.update.message.text;
-    let [courseCode, biddingWindow, instructor] = userInput.split(", ");
+
+// User chose Option 1 from bidsInText
+const bidsInTextOption1 = new Scenes.BaseScene("bidsInTextOption1");
+
+bidsInTextOption1.enter(ctx => {
+    ctx.replyWithHTML("To check for bids, please send me: term, course code, bidding window, instructor.\nExample: 2021-22 Term 2, IS215, R1BW1, Christopher Michael Poskitt.")
+})
+
+bidsInTextOption1.on("text", async (ctx) => {
     try {
         const results = await axios.get("http://localhost:5000/api/bidding", {
-            params: {
-                courseCode: courseCode,
-                biddingWindow: biddingWindow,
-                instructor: instructor.toUpperCase()
-            }
+            params: { userInput: ctx.update.message.text }
         });
-        
-        let msgReply = "Results for your query: \n";
-        for (let result of results.data) {
-            msgReply += `Section: ${result.section}\nVacancy: ${result.vacancy}\n Opening Vacancy: ${result.openingVacancy}\nBefore Process Vacancy: ${result.beforeProcessVacancy}\nAfter Process Vacancy: ${result.afterProcessVacancy}\nMedian Bid: ${result.medianBid}\nMin Bid: ${result.minBid}\nEnrolled: ${result.enrolledStudents}`
-            
-            msgReply += "\n\n"
+        let msgReply = "Results for your query: \n\n";
+        if (results.data.length !== 0) {
+            for (let result of results.data) {
+                msgReply += `<u>Section ${result.section}</u>\nClass Size: ${result.enrolledStudents} out of ${result.vacancy}\nOpening vacancy: ${result.openingVacancy}\nMedian Bid: ${result.medianBid}\nMin Bid: ${result.minBid}`;
+                
+                msgReply += "\n\n";
+            }
+        } else {
+            msgReply += "No results found.";
         }
-        ctx.replyWithHTML(msgReply);
+        ctx.replyWithHTML(msgReply, 
+            Markup.inlineKeyboard([
+                Markup.button.callback("Back", "back"),
+                Markup.button.callback("Search again", "again")
+            ])
+        );
     } catch (error) {
-        console.log(error)
+        ctx.reply(`An error has occured. Please try again or contact our admin. Error message: ${error}`);
     }
 });
 
+bidsInTextOption1.action("back", enter("bidsInText"));
+bidsInTextOption1.action("again", enter("bidsInTextOption1"));
+bidsInTextOption1.command("leave", ctx => {
+    ctx.reply("Bye bye! Thanks for using our bot.")
+    ctx.scene.leave()
+});
+
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+// Staging the scenes
+const stage = new Scenes.Stage([bidsInText, bidsInTextOption1])
+bot.use(session())
+bot.use(stage.middleware())
+
+bot.start(ctx => {
+    ctx.replyWithHTML("Welcome to SMU Bid Checker bot. What do you want to do today?", Markup.inlineKeyboard([
+        Markup.button.callback("Bids in Text", "bidsInText"),
+        Markup.button.callback("Bids in Graph", "bidsInGraph")
+    ]))
+});
+bot.command("leave", ctx => {
+    ctx.reply("Bye bye! Thanks for using our bot.");
+    ctx.leaveChat();
+})
+bot.action("bidsInText", ctx => ctx.scene.enter("bidsInText"));
 bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
