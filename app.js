3f7963d7aca5142d29da4be95cd061c41af50741
jonjonnyjonjon <70429099+jonjonnyjonjon@users.noreply.github.com>
2021-12-30 21:13:21 +0800

    refactor: deleted dead codes and reordered codes

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const path = require("path");
 
 const { enter, leave } = Scenes.Stage;
 
+function formatUserInput(userInput) {
+    let formattedUserInput = userInput.split(", ");
+    let bwSplit = formattedUserInput[2].split("R").join("").split("W");
+    formattedUserInput[2] = `Round ${bwSplit[0]} Window ${bwSplit[1]}`;
+    return formattedUserInput
+}
+
 // User chose Text Bid Results
 const bidsInText = new Scenes.BaseScene("bidsInText");
 
@@ -42,17 +49,11 @@ bidsInTextOption1.command("leave", ctx => {
 bidsInTextOption1.on("text", async (ctx) => {
     let [term, courseCode, biddingWindow, instructor] = formatUserInput(ctx.update.message.text)
     try {
-        const userInput = ctx.update.message.text;
-        let [term, courseCode, biddingWindow, instructor] = userInput.split(", ");
-        let bwSplit = biddingWindow.split("R").join("").split("W");
-        let bwFormatted = `Round ${bwSplit[0]} Window ${bwSplit[1]}`;
-        // check length must be == 4
-        // use a constants.js to export then import here to check the full spelling of R1W1
         const results = await axios.get("http://localhost:5000/api/bidding", {
             params: {
                 term: term,
                 courseCode: courseCode.toUpperCase(),
-                biddingWindow: bwFormatted,
+                biddingWindow: biddingWindow,
                 instructor: instructor.toUpperCase()
             }
         });
@@ -75,6 +76,81 @@ bidsInTextOption1.on("text", async (ctx) => {
     } catch (error) {
         ctx.reply(`An error has occured. Please try again or contact our admin. Error message: ${error}`);
     }
+});
+
+// User chose Option 2 from bidsInText
+const bidsInTextOption2 = new Scenes.BaseScene("bidsInTextOption2");
+
+bidsInTextOption2.enter(ctx => {
+    ctx.replyWithHTML("To check for bids, please send me: term, course code and bidding window.\n\nExample: <b>2021-22 Term 2, IS215, R1BW1</b>\n\nI will reply with a list of instructors for you to choose from!")
+})
+
+bidsInTextOption2.on("text", async (ctx) => {
+    if (ctx.update.message.text.indexOf(', ') != -1) {
+        let [term, courseCode, biddingWindow] = formatUserInput(ctx.update.message.text)
+        ctx.scene.session.currInput = ctx.update.message.text;
+        try {
+            const results = await axios.get("http://localhost:5000/api/instructors", {
+                params: {
+                    term: term,
+                    courseCode: courseCode,
+                    biddingWindow: biddingWindow
+                }
+            });
+            let msgReply = "To proceed, please select one of the following option numbers: \n\n";
+            if (results.data.length !== 0) {
+                ctx.scene.session.instructorArr = [];
+                for (let i = 0; i < results.data.length; i++) {
+                    msgReply += `/${i + 1} ${results.data[i]}\n`
+                    ctx.scene.session.instructorArr.push(results.data[i])
+                }
+            } else {
+                msgReply += "No results found, please try again!";
+            }
+            ctx.replyWithHTML(msgReply);
+        } catch (error) {
+            ctx.reply(`An error has occured. Please try again or contact our admin. Error message: ${error}`);
+        }
+    } else {
+        let instructorIdx = parseInt(ctx.update.message.text.substring(1)) - 1;
+        let userInput = ctx.scene.session.currInput + ", " + ctx.scene.session.instructorArr[instructorIdx];
+        let [term, courseCode, biddingWindow, instructor] = formatUserInput(userInput)
+        try {
+            const results = await axios.get("http://localhost:5000/api/bidding", {
+                params: {
+                    term: term,
+                    courseCode: courseCode,
+                    biddingWindow: biddingWindow,
+                    instructor: instructor
+                }
+            });
+            let msgReply = "Results for your query: \n\n";
+            if (results.data.length !== 0) {
+                for (let result of results.data) {
+                    msgReply += `<u>Section ${result.section}</u>\nClass Size: ${result.enrolledStudents} out of ${result.vacancy}\nOpening vacancy: ${result.openingVacancy}\nMedian Bid: ${result.medianBid}\nMin Bid: ${result.minBid}`;
+
+                    msgReply += "\n\n";
+                }
+            } else {
+                msgReply += "No results found.";
+            }
+            ctx.replyWithHTML(msgReply,
+                Markup.inlineKeyboard([
+                    Markup.button.callback("Back", "back"),
+                    Markup.button.callback("Search again", "again")
+                ])
+            );
+        } catch (error) {
+            ctx.reply(`An error has occured. Please try again or contact our admin. Error message: ${error}`);
+        }
+    }
+});
+
+bidsInTextOption2.action("back", enter("bidsInText"));
+bidsInTextOption2.action("again", enter("bidsInTextOption2"));
+bidsInTextOption2.command("leave", ctx => {
+    ctx.reply("Bye bye! Thanks for using our bot.")
+    ctx.scene.leave()
 });
 
 // User chose Text Bid Results
@@ -171,89 +247,6 @@ bidsInGraph.on("text", async (ctx) => {
         ctx.reply(`An error has occured. Please try again or contact our admin. Error message: ${error}`);
     }
 })
-
-// User chose Option 2 from bidsInText
-const bidsInTextOption2 = new Scenes.BaseScene("bidsInTextOption2");
-
-bidsInTextOption2.enter(ctx => {
-    ctx.replyWithHTML("To check for bids, please send me: term, course code and bidding window.\n\nExample: <b>2021-22 Term 2, IS215, R1BW1</b>\n\nI will reply with a list of instructors for you to choose from!")
-})
-
-bidsInTextOption2.on("text", async (ctx) => {
-    if (ctx.update.message.text.indexOf(', ') != -1) {
-        let [term, courseCode, biddingWindow] = formatUserInput(ctx.update.message.text)
-        ctx.scene.session.currInput = ctx.update.message.text;
-        try {
-            const results = await axios.get("http://localhost:5000/api/instructors", {
-                params: {
-                    term: term,
-                    courseCode: courseCode,
-                    biddingWindow: biddingWindow
-                }
-            });
-            let msgReply = "To proceed, please select one of the following option numbers: \n\n";
-            if (results.data.length !== 0) {
-                ctx.scene.session.instructorArr = [];
-                for (let i = 0; i < results.data.length; i++) {
-                    msgReply += `/${i + 1} ${results.data[i]}\n`
-                    ctx.scene.session.instructorArr.push(results.data[i])
-                }
-            } else {
-                msgReply += "No results found, please try again!";
-            }
-            ctx.replyWithHTML(msgReply);
-        } catch (error) {
-            ctx.reply(`An error has occured. Please try again or contact our admin. Error message: ${error}`);
-        }
-    } else {
-        let instructorIdx = parseInt(ctx.update.message.text.substring(1)) - 1;
-        let userInput = ctx.scene.session.currInput + ", " + ctx.scene.session.instructorArr[instructorIdx];
-        let [term, courseCode, biddingWindow, instructor] = formatUserInput(userInput)
-        try {
-            const results = await axios.get("http://localhost:5000/api/bidding", {
-                params: {
-                    term: term,
-                    courseCode: courseCode,
-                    biddingWindow: biddingWindow,
-                    instructor: instructor
-                }
-            });
-            let msgReply = "Results for your query: \n\n";
-            if (results.data.length !== 0) {
-                for (let result of results.data) {
-                    msgReply += `<u>Section ${result.section}</u>\nClass Size: ${result.enrolledStudents} out of ${result.vacancy}\nOpening vacancy: ${result.openingVacancy}\nMedian Bid: ${result.medianBid}\nMin Bid: ${result.minBid}`;
-
-                    msgReply += "\n\n";
-                }
-            } else {
-                msgReply += "No results found.";
-            }
-            ctx.replyWithHTML(msgReply,
-                Markup.inlineKeyboard([
-                    Markup.button.callback("Back", "back"),
-                    Markup.button.callback("Search again", "again")
-                ])
-            );
-        } catch (error) {
-            ctx.reply(`An error has occured. Please try again or contact our admin. Error message: ${error}`);
-        }
-    }
-});
-
-function formatUserInput(userInput) {
-    let formattedUserInput = userInput.split(", ");
-    let bwSplit = formattedUserInput[2].split("R").join("").split("W");
-    formattedUserInput[2] = `Round ${bwSplit[0]} Window ${bwSplit[1]}`;
-    return formattedUserInput
-}
-
-bidsInTextOption2.action("back", enter("bidsInText"));
-bidsInTextOption2.action("again", enter("bidsInTextOption2"));
-bidsInTextOption2.command("leave", ctx => {
-    ctx.reply("Bye bye! Thanks for using our bot.")
-    ctx.scene.leave()
-});
-
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
