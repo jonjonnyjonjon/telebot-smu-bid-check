4d3109e46a889e95c5866ac7dcda81793bfbcf28
jonjonnyjonjon <70429099+jonjonnyjonjon@users.noreply.github.com>
2022-01-07 11:39:17 +0800

    feat: graph option 2

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ require("dotenv").config();
 const { Telegraf, Scenes, Markup, session } = require("telegraf");
 
 const axios = require("axios");
-const ChartJSImage = require('chart.js-image');
+const QuickChart = require("quickchart-js");
+
 const fs = require("fs");
 const path = require("path");
 
-const { enter, leave } = Scenes.Stage;
+const { enter } = Scenes.Stage;
 
 function formatUserInput(userInput) {
     let formattedUserInput = userInput.split(", ");
@@ -19,7 +20,7 @@ function formatUserInput(userInput) {
 const bidsInText = new Scenes.BaseScene("bidsInText");
 
 bidsInText.enter(ctx => {
-    ctx.replyWithHTML("How do you want to find your bids?\nOption 1: Specific term, course, bidding window and instructor\nOption 2: Specific term, course and bidding window", Markup.inlineKeyboard([
+    ctx.replyWithHTML("How do you want to look at past bids?\nOption 1: Based on the specific term, course code, bidding window and instructor\nOption 2: Based on the specific term, course and bidding window", Markup.inlineKeyboard([
         Markup.button.callback("Option 1", "option1"),
         Markup.button.callback("Option 2", "option2")
     ]))
@@ -29,7 +30,7 @@ bidsInText.action("option1", enter("bidsInTextOption1"))
 bidsInText.action("option2", enter("bidsInTextOption2"))
 bidsInText.command("leave", ctx => {
     ctx.reply("Bye bye! Thanks for using our bot.");
-    leave();
+    ctx.scene.leave();
 });
 
 // User chose Option 1 from bidsInText
@@ -42,8 +43,8 @@ bidsInTextOption1.enter(ctx => {
 bidsInTextOption1.action("back", enter("bidsInText"));
 bidsInTextOption1.action("again", enter("bidsInTextOption1"));
 bidsInTextOption1.command("leave", ctx => {
-    ctx.reply("Bye bye! Thanks for using our bot.")
-    leave()
+    ctx.reply("Bye bye! Thanks for using our bot.");
+    ctx.scene.leave();
 });
 
 bidsInTextOption1.on("text", async (ctx) => {
@@ -74,7 +75,7 @@ bidsInTextOption1.on("text", async (ctx) => {
             ])
         );
     } catch (error) {
-        ctx.reply(`An error has occured. Please try again or contact our admin. Error message: ${error}`);
+        ctx.reply(`An error has occured. Error message: ${error}. Please contact our admin with this issue, or you may proceed to search again or leave the bot.`);
     }
 });
 
@@ -88,8 +89,8 @@ bidsInTextOption2.enter(ctx => {
 bidsInTextOption2.action("back", enter("bidsInText"));
 bidsInTextOption2.action("again", enter("bidsInTextOption2"));
 bidsInTextOption2.command("leave", ctx => {
-    ctx.reply("Bye bye! Thanks for using our bot.")
-    ctx.scene.leave()
+    ctx.reply("Bye bye! Thanks for using our bot.");
+    ctx.scene.leave();
 });
 
 bidsInTextOption2.hears(/^\/[0-9]+/, async ctx => {
@@ -125,7 +126,7 @@ bidsInTextOption2.hears(/^\/[0-9]+/, async ctx => {
                 ])
             );
         } catch (error) {
-            ctx.reply(`An error has occured. Please try again or contact our admin. Error message: ${error}`);
+            ctx.reply(`An error has occured. Error message: ${error}. Please contact our admin with this issue, or you may proceed to search again or leave the bot.`);
         }
     }
 });
@@ -153,7 +154,7 @@ bidsInTextOption2.on("text", async (ctx) => {
         }
         ctx.replyWithHTML(msgReply);
     } catch (error) {
-        ctx.reply(`An error has occured. Please try again or contact our admin. Error message: ${error}`);
+        ctx.reply(`An error has occured. Error message: ${error}. Please contact our admin with this issue, or you may proceed to search again or leave the bot.`);
     }
 });
 
@@ -161,19 +162,35 @@ bidsInTextOption2.on("text", async (ctx) => {
 const bidsInGraph = new Scenes.BaseScene("bidsInGraph");
 
 bidsInGraph.enter(ctx => {
-    ctx.replyWithHTML("To generate a graph with bids, please send me: term, course code, instructor and section.\nExample: 2021-22 Term 2, IS215, Christopher Michael Poskitt, G7");
+    ctx.replyWithHTML("To generate a graph with bids, please either of the following to continue:\nOption 1: View past bids by their term, course code, instructor and section\nOption 2: View all past bids by term and course code",
+    Markup.inlineKeyboard([
+        Markup.button.callback("Option 1", "option1"),
+        Markup.button.callback("Option 2", "option2")
+    ]));
 });
-
+bidsInGraph.action("option1", enter("bidsInGraphOption1"));
+bidsInGraph.action("option2", enter("bidsInGraphOption2"));
 bidsInGraph.command("leave", ctx => {
-    ctx.reply("Bye bye! Thanks for using our bot.")
-    leave()
+    ctx.reply("Bye bye! Thanks for using our bot.");
+    ctx.scene.leave();
 });
 
-bidsInGraph.on("text", async (ctx) => {
+// User choose bidsInGraph - Option 1
+const bidsInGraphOption1 = new Scenes.BaseScene("bidsInGraphOption1");
+bidsInGraphOption1.enter(ctx => {
+    ctx.replyWithHTML("Please send me the term, course code, instructor and section to generate the graph.\nExample: 2021-22 Term 2, IS215, Christopher Michael Poskitt, G7");
+})
+bidsInGraphOption1.action("back", enter("bidsInGraph"));
+bidsInGraphOption1.action("leave", ctx => {
+    ctx.reply("Bye bye! Thanks for using our bot.");
+    ctx.scene.leave();
+})
+
+bidsInGraphOption1.on("text", async (ctx) => {
     try {
         const userInput = ctx.update.message.text;
         let [term, courseCode, instructor, section] = userInput.split(", ");
-        const results = await axios.get("http://localhost:5000/api/forGraph", {
+        const results = await axios.get("http://localhost:5000/api/graphOption1", {
             params: {
                 term: term,
                 courseCode: courseCode.toUpperCase(),
@@ -181,6 +198,11 @@ bidsInGraph.on("text", async (ctx) => {
                 section: section.toUpperCase()
             }
         });
+
+        if (results.data.length === 0) {
+            ctx.reply("There is either no such course or no bids history for this course. Please try again or leave the bot to restart.");
+            return;
+        }
 
         let biddingWindows = [];
         let minBids = [];
@@ -190,72 +212,176 @@ bidsInGraph.on("text", async (ctx) => {
             minBids.push(result.minBid);
             medianBids.push(results.medianBid);
         }
+        biddingWindows = biddingWindows.reverse();
 
-        const line_chart = ChartJSImage().chart({
-            "type": "line",
-            "data": {
-                "labels": biddingWindows,
-                "datasets": [
+        const myChart = new QuickChart();
+        myChart.setConfig({
+            type: "bar",
+            data: {
+                labels: biddingWindows,
+                datasets: [
                     {
-                        "label": "Min Bids",
-                        "borderColor": "rgb(255,+99,+132)",
-                        "backgroundColor": "rgba(255,+99,+132,+.5)",
-                        "data": minBids
+                        type: "bar",
+                        label: "Min bids",
+                        backgroundColor: "rgba(255, 99, 132, 0.5)",
+                        borderColor: "rgb(255, 99, 132)",
+                        data: minBids
                     },
                     {
-                        "label": "Median Bids",
-                        "borderColor": "rgb(123,+99,+432)",
-                        "backgroundColor": "rgba(255,+99,+132,+.5)",
-                        "data": minBids
+                        type: "line",
+                        label: "Median bids",
+                        backgroundColor: "rgba(75, 192, 192, 0.5)",
+                        borderColor: "rgb(75, 192, 192)",
+                        fill: false,
+                        data: minBids
                     }
                 ]
             },
-            "options": {
-                "title": {
-                    "display": true,
-                    "text": "Your results"
-                },
-                "scales": {
-                    "yAxes": [
-                        {
-                            "stacked": true,
-                            "scaleLabel": {
-                                "display": true,
-                                "labelString": "e$"
-                            }
-                        }
-                    ]
+            options: {
+                title: {
+                    text: "Your results"
                 }
             }
-        })
-            .backgroundColor('white')
-            .width(500)
-            .height(300);
-        line_chart.toURL();
-        await line_chart.toFile("bidChart.png");
-        await line_chart.toDataURI();
-        await line_chart.toBuffer();
+        });
+        await myChart.toFile(path.join(__dirname, "bidGraphOption1.png"));
 
         ctx.replyWithPhoto(
-            { source: path.join(__dirname, "bidChart.png") },
-            { caption: "This is your graph." }
+            { source: path.join(__dirname, "bidGraphOption1.png") },
+            { 
+                caption: "This is your graph.",
+                parse_mode: "Markdown",
+                ...Markup.inlineKeyboard([
+                    Markup.button.callback("Back", "back"),
+                    Markup.button.callback("Leave", "leave")
+                ])
+            }
         );
 
-        fs.unlink(path.join(__dirname, "bidChart.png"), err => {
+        fs.unlink(path.join(__dirname, "bidGraphOption1.png"), err => {
             if (err) console.log(err)
             else {
-                console.log("Deleted file: " + path.join(__dirname, "bidChart.png"))
+                console.log("Deleted file: " + path.join(__dirname, "bidGraphOption1.png"))
             }
         });
     } catch (error) {
-        ctx.reply(`An error has occured. Please try again or contact our admin. Error message: ${error}`);
+        ctx.reply(`An error has occured. Error message: ${error}. Please contact our admin with this issue, or you may proceed to search again or leave the bot.`);
     }
 })
+
+// User choose bidsInGraph - Option 2
+const bidsInGraphOption2 = new Scenes.BaseScene("bidsInGraphOption2");
+
+bidsInGraphOption2.action("back", enter("bidsInGraph"));
+bidsInGraphOption2.action("leave", ctx => {
+    ctx.reply("Bye bye! Thanks for using our bot.");
+    ctx.scene.leave();
+});
+
+bidsInGraphOption2.enter(ctx => {
+    ctx.replyWithHTML("Please send me the term, course code, type of bid to generate the graph.\nExample: 2021-22 Term 2, IS215, median/min");
+});
+bidsInGraphOption2.on("text", async (ctx) => {
+    try {
+        const userInput = ctx.update.message.text;
+        let [term, courseCode, metric] = userInput.split(", ");
+        const results = await axios.get("http://localhost:5000/api/graphOption2", {
+            params: {
+                term: term,
+                courseCode: courseCode.toUpperCase()
+            }
+        });
+
+        if (results.data.length === 0) {
+            ctx.reply("There is either no such course or no bids history for this course. Please try again or leave the bot to restart.");
+            return;
+        }
+
+        let sortedResults = {};
+        for (let result of results.data) {
+            if (result.instructor in sortedResults) {
+                if (result.section in sortedResults[result.instructor]) {
+                    sortedResults[result.instructor][result.section]["biddingWindows"].push(result.biddingWindow);
+                    sortedResults[result.instructor][result.section]["minBids"].push(result.minBid);
+                    sortedResults[result.instructor][result.section]["medianBids"].push(result.medianBid);
+                } else {
+                    sortedResults[result.instructor][result.section] = {
+                        biddingWindows: [result.biddingWindow],
+                        minBids: [result.minBid],
+                        medianBids: [result.medianBid]
+                    };
+                }
+            } else {
+                sortedResults[result.instructor] = {};
+                sortedResults[result.instructor][result.section] = {
+                    biddingWindows: [result.biddingWindow],
+                    minBids: [result.minBid],
+                    medianBids: [result.medianBid]
+                };
+            }
+        }
+
+        let dataConfigs = [];
+        let biddingWindows;
+        for (let instructor in sortedResults) {
+            for (let section in sortedResults[instructor]) {
+                let sectionInfo = sortedResults[instructor][section];
+                if (!biddingWindows) {
+                    biddingWindows = sectionInfo.biddingWindows.reverse();
+                }
+
+                let dataset = {
+                    type: "line",
+                    label: `${section}(${instructor})`,
+                    fill: false,
+                    borderColor: "#" + Math.floor(Math.random()*16777215).toString(16),
+                    data: metric == "median" ? sectionInfo.medianBids.reverse() : sectionInfo.minBids.reverse()
+                };
+                dataConfigs.push(dataset);
+            }
+        }
+
+        const myChart = new QuickChart();
+        myChart.setConfig({
+            type: "line",
+            data: {
+                labels: biddingWindows,
+                datasets: dataConfigs
+            },
+            options: {
+                title: {
+                    text: `Your results for ${metric} bid`
+                }
+            }
+        });
+        await myChart.toFile(path.join(__dirname, "bidGraphOption2.png"));
+
+        ctx.replyWithPhoto(
+            { source: path.join(__dirname, "bidGraphOption2.png") },
+            { 
+                caption: "This is your graph.",
+                parse_mode: "Markdown",
+                ...Markup.inlineKeyboard([
+                    Markup.button.callback("Back", "back"),
+                    Markup.button.callback("Leave", "leave")
+                ])
+            }
+        );
+
+        fs.unlink(path.join(__dirname, "bidGraphOption2.png"), err => {
+            if (err) console.log(err)
+            else {
+                console.log("Deleted file: " + path.join(__dirname, "bidGraphOption2.png"))
+            }
+        });
+    } catch (error) {
+        ctx.reply(`An error has occured. Error message: ${error}. Please contact our admin with this issue, or you may proceed to search again or leave the bot.`);
+    }
+});
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 // Staging the scenes
-const stage = new Scenes.Stage([bidsInText, bidsInTextOption1, bidsInTextOption2, bidsInGraph])
+const stage = new Scenes.Stage([bidsInText, bidsInTextOption1, bidsInTextOption2, bidsInGraph, bidsInGraphOption1, bidsInGraphOption2])
 bot.use(session())
 bot.use(stage.middleware())
 
